cb2e54e4de3aff24bd3785d809a4d0d88e3e167b
alioune Balde <aliounebalde@esp.sn>
2024-09-06 17:35:14 +0000

    refactor: optimizing the code for less query to the databse and more readablity

--- element.sh
+++ element.sh
@@ -1,66 +1,67 @@
 #!/bin/bash
 PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
-print(){
-if [[ $# != 8 ]];then
-  echo "I need all  arguments!"
-else
-    echo "The element with atomic number $1 is $2 ($3). It's a $4, with a mass of $5 amu. $6 has a melting point of $7 celsius and a boiling point of $8 celsius."
-fi
+
+# Fonction pour afficher les informations d'un élément
+print_element_info() {
+  local atomic_number=$1
+  element_data=$($PSQL "SELECT e.name, e.symbol, t.type, p.atomic_mass, p.melting_point_celsius, p.boiling_point_celsius 
+                        FROM elements e 
+                        INNER JOIN properties p ON e.atomic_number = p.atomic_number 
+                        INNER JOIN types t ON p.type_id = t.type_id 
+                        WHERE e.atomic_number = $atomic_number")
+  
+  # Extraire les données retournées
+  IFS="|" read -r name symbol type atomic_mass melting_point boiling_point <<< "$element_data"
+
+  # Imprimer les informations formatées
+  echo "The element with atomic number $atomic_number is $name ($symbol). It's a $type, with a mass of $atomic_mass amu. $name has a melting point of $melting_point celsius and a boiling point of $boiling_point celsius."
 }
 
+# Vérification des arguments
 if [[ -z $1 ]]; then
   echo "Please provide an element as an argument."
-
-#si l'argument passé est un nbre atomic
-elif [[ $1 =~ ^[0-9]+$ ]]; then
-
-  atomic_number=$($PSQL "select atomic_number from elements where atomic_number =$1")
-#test if that element exist
-if [[ -z $atomic_number ]];then
-  echo "I could not find that element in the database."
   exit 1
 fi
-  type=$($PSQL "select t.type from types t inner join properties p on t.type_id=p.type_id where p.atomic_number=$atomic_number")
-  symbol=$($PSQL "select symbol from elements where atomic_number=$atomic_number")
-  name=$($PSQL "select name from elements where atomic_number=$atomic_number")
-  mass=$($PSQL "select atomic_mass from properties where atomic_number=$atomic_number")
-  melting_point=$($PSQL "select melting_point_celsius from properties where atomic_number=$atomic_number")
-  boiling_point=$($PSQL "select boiling_point_celsius from properties where atomic_number=$atomic_number")
-  print $atomic_number $name $symbol $type $mass $name $melting_point $boiling_point
 
-#verifier si l'argument est un symbol
-elif [[ "$1" =~ ^[a-zA-Z]+$ && ${#1} -le 2 ]];then
-  symbol=$1
-  atomic_number=$($PSQL "select atomic_number from elements where symbol ='$symbol'")
-  #test if the element exist
-  if [[ -z $atomic_number ]];then
-    echo "I could not find that element in the database."
-    exit 1
-  fi
-  type=$($PSQL "select t.type from types t inner join properties p on t.type_id=p.type_id where p.atomic_number=$atomic_number")
-  name=$($PSQL "select name from elements where symbol ='$symbol'")
-  mass=$($PSQL "select atomic_mass from properties where atomic_number=$atomic_number")
-  melting_point=$($PSQL "select melting_point_celsius from properties where atomic_number=$atomic_number")
-  boiling_point=$($PSQL "select boiling_point_celsius from properties where atomic_number=$atomic_number")
+# Si l'argument est un nombre (numéro atomique)
+if [[ $1 =~ ^[0-9]+$ ]]; then
+  atomic_number=$($PSQL "SELECT atomic_number FROM elements WHERE atomic_number = $1")
   
-  print $atomic_number $name $symbol $type $mass $name $melting_point $boiling_point
-
-#si l'argument passé est le nom de l'élément
-elif [[ "$1" =~ ^[a-zA-Z]+$ && ${#1} -gt 2 ]];then
-  name=$1
-  atomic_number=$($PSQL "select atomic_number from elements where name ='$name'")
-  #test if the element exist
-  if [[ -z $atomic_number ]];then
+  # Vérification si l'élément existe
+  if [[ -z $atomic_number ]]; then
     echo "I could not find that element in the database."
     exit 1
   fi
-  type=$($PSQL "select t.type from types t inner join properties p on t.type_id=p.type_id where p.atomic_number=$atomic_number")
-  symbol=$($PSQL "select symbol from elements where atomic_number =$atomic_number")
-  mass=$($PSQL "select atomic_mass from properties where atomic_number=$atomic_number")
-  melting_point=$($PSQL "select melting_point_celsius from properties where atomic_number=$atomic_number")
-  boiling_point=$($PSQL "select boiling_point_celsius from properties where atomic_number=$atomic_number")
-  print $atomic_number $name $symbol $type $mass $name $melting_point $boiling_point
+  
+  # Appeler la fonction pour afficher les infos de l'élément
+  print_element_info $atomic_number
+
+# Si l'argument est un symbole (2 caractères max)
+elif [[ "$1" =~ ^[a-zA-Z]+$ && ${#1} -le 2 ]]; then
+  symbol=$1
+  atomic_number=$($PSQL "SELECT atomic_number FROM elements WHERE symbol = '$symbol'")
+  
+  if [[ -z $atomic_number ]]; then
+    echo "I could not find that element in the database."
+    exit 1
+  fi
+  
+  # Afficher les infos de l'élément
+  print_element_info $atomic_number
+
+# Si l'argument est un nom d'élément
+elif [[ "$1" =~ ^[a-zA-Z]+$ && ${#1} -gt 2 ]]; then
+  name=$1
+  atomic_number=$($PSQL "SELECT atomic_number FROM elements WHERE name = '$name'")
+  
+  if [[ -z $atomic_number ]]; then
+    echo "I could not find that element in the database."
+    exit 1
+  fi
+  
+  # Afficher les infos de l'élément
+  print_element_info $atomic_number
+
 else
   echo "I could not find that element in the database."
 fi
-
